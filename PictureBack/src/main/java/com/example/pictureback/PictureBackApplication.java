75afca27fc0a55c18bfe1a4b2efb1cb1355086a4
y-tanx <myc55945752@163.com>
2025-05-22 15:00:40 +0800

    init

---
+++ PictureBack/src/main/java/com/example/pictureback/PictureBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pictureback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureBackApplication.class, args);
+    }
+
+}
---
+++ PictureBack/src/test/java/com/example/pictureback/PictureBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pictureback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
